fdc752a7bd5f3b7cfaaf0249f5bbcdc62351db91
abdullahshah02 <abdullah.shah877@gmail.com>
2020-08-09 12:31:41 +0500

    added env file

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	env: {
+		baseURL: 'http://localhost:3000',
+		pythonPath: 'C:\\Python\\python.exe', //path where python is installed
+		scriptDir: 'F:\\Fiverr\\nextGW\\Autohotspot' // make sure you use an absolute path for scriptPath
+	}
+}
