7f58870e3346c6fb4ad406d002d7ae6fe76a181e
abdullahshah02 <abdullah.shah877@gmail.com>
2020-08-14 17:39:34 +0500

    added mobile and desktop links to navbar

--- Components/Navigation/Navbar.js
+++ Components/Navigation/Navbar.js
@@ -16,21 +16,44 @@ const useStyles = makeStyles((theme) => ({
       flexDirection: "row",
     },
   },
-  paper: {
+  PaperDesk: {
     padding: theme.spacing(2),
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "#dae3f0",
-    flexDirection: 'column',
-    borderRadius: "20px",
-    // boxShadow: "4px 2px 11px grey",
-    cursor: "pointer",
+    textAlign: "center",
     fontFamily: "Segoe UI ",
-    fontSize: "16px",
+    fontSize: "20px",
     color: "#7e7e7e",
+    backgroundColor: "#dae3f0",
+    borderRadius: "8px",
+    //boxShadow: "5px 6px 13px  grey",
+    cursor: "pointer",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
     [theme.breakpoints.down("xs")]: {
       padding: theme.spacing(4),
+      fontSize: "24px",
+      display: "none",
+    },
+  },
+  PaperMob: {
+    display: "none",
+    padding: theme.spacing(2),
+    textAlign: "center",
+    fontFamily: "Segoe UI ",
+    fontSize: "20px",
+    color: "#7e7e7e",
+    backgroundColor: "#dae3f0",
+    borderRadius: "8px",
+    //boxShadow: "5px 6px 13px  grey",
+    cursor: "pointer",
+    flexDirection: "column",
+    [theme.breakpoints.down("xs")]: {
+      padding: theme.spacing(4),
+      fontSize: "20px",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
     },
   },
   Img: {
@@ -57,7 +80,21 @@ export default function Navbar() {
     <div className={classes.root}>
       <Grid className={classes.grid} container spacing={3}>
         <Grid className={classes.Centerbox} item xs={6} sm={12}>
-          <Paper className={classes.paper} onClick={() => Router.push("/wifi")}>
+          <Paper
+            onClick={() => Router.push("/")}
+            className={classes.PaperDesk}
+          >
+            <img
+              className={classes.Img}
+              src="/wifi_vectorized.png"
+              alt="WiFi Logo"
+            />
+            WIFI
+          </Paper>
+          <Paper
+            onClick={() => Router.push("/wifi")}
+            className={classes.PaperMob}
+          >
             <img
               className={classes.Img}
               src="/wifi_vectorized.png"
@@ -67,7 +104,17 @@ export default function Navbar() {
           </Paper>
         </Grid>
         <Grid className={classes.Centerbox} item xs={6} sm={12}>
-          <Paper className={classes.paper}>
+          <Paper className={classes.PaperDesk}>
+            <a href="http://a57f3ce3144e.ngrok.io/pikrellcam/media/videos/raspberrypi_video_2019-05-13_18.35.54.mp4">
+              <img
+                className={classes.Img}
+                src="/video.png"
+                alt="Video Logo"
+              />
+            </a>
+            VIDEOS
+          </Paper>
+          <Paper className={classes.PaperMob}>
             <a href="http://a57f3ce3144e.ngrok.io/pikrellcam/media/videos/raspberrypi_video_2019-05-13_18.35.54.mp4">
               <img
                 className={classes.Img}
@@ -79,7 +126,31 @@ export default function Navbar() {
           </Paper>
         </Grid>
         <Grid className={classes.Centerbox} item xs={6} sm={12}>
-          <Paper className={classes.paper} onClick={() => Router.push("/wifi")}>
+          <Paper
+            onClick={() => Router.push("/livestream")}
+            className={classes.PaperDesk}
+          >
+            <img
+              className={classes.Img}
+              src="/wifi_vectorized.png"
+              alt="WiFi Logo"
+            />
+            LIVE
+          </Paper>
+          <Paper
+            onClick={() => Router.push("/livestream")}
+            className={classes.PaperMob}
+          >
+            <img
+              className={classes.Img}
+              src="/wifi_vectorized.png"
+              alt="WiFi Logo"
+            />
+            LIVE
+          </Paper>
+        </Grid>
+        <Grid className={classes.Centerbox} item xs={6} sm={12}>
+          <Paper className={classes.PaperDesk}>
             <img
               className={classes.Img}
               src="/wifi_vectorized.png"
@@ -87,9 +158,10 @@ export default function Navbar() {
             />
             WIFI
           </Paper>
-        </Grid>
-        <Grid className={classes.Centerbox} item xs={6} sm={12}>
-          <Paper className={classes.paper} onClick={() => Router.push("/wifi")}>
+          <Paper
+            onClick={() => Router.push("/wifi")}
+            className={classes.PaperMob}
+          >
             <img
               className={classes.Img}
               src="/wifi_vectorized.png"
