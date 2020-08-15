c08239e3ab6b9cf47d66754d282d422418970a01
abdullahshah02 <abdullah.shah877@gmail.com>
2020-08-15 19:52:25 +0500

    added mobile version of livestream

--- pages/livestream.js
+++ pages/livestream.js
@@ -15,7 +15,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     textAlign: "center",
     width: "70%",
-    fontFamily: "Segoe UI ",
+    fontFamily: "Segoe UI",
     fontSize: "20px",
     color: "#7e7e7e",
     backgroundColor: "#dae3f0",
@@ -35,7 +35,11 @@ const useStyles = makeStyles((theme) => ({
   main: {
     padding: "4rem",
     [theme.breakpoints.down("xs")]: {
-      display: "none",
+      padding: "1rem",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column"
     },
   },
   offline: {
@@ -55,12 +59,40 @@ const useStyles = makeStyles((theme) => ({
       display: "block",
     },
   },
-  WiFi: {
-    padding: "1rem 5rem",
+  box: {
+    border: "1px ",
+    boxShadow: "1px 1px 4px grey",
+    backgroundColor: "#f2f2f2",
+    borderRadius: "1rem",
+    height: "23rem",
+    width: "30rem",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
     [theme.breakpoints.down("sm")]: {
-      padding: "0",
+      width: "20rem",
     },
   },
+  video: {
+    width: "100%",
+    height: "86.5%",
+    padding: theme.spacing(2),
+    backgroundColor: "#dae3f0",
+    borderRadius: "10px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "17px",
+    [theme.breakpoints.down("xs")]: {
+      height: '250px',
+    },
+  },
+  header: {
+    [theme.breakpoints.down("xs")]: {
+      display: "none"
+    },
+  }
 }));
 
 export default function Livestream({ baseURL }) {
@@ -74,14 +106,17 @@ export default function Livestream({ baseURL }) {
 
   return (
     <Container>
-      <Grid className={classes.grid} container spacing={5}>
-        <Grid item xs={12} sm={3} md={2}>
-          <Navbar />
+      <Grid className={classes.grid} container>
+        <Grid item xs={12} sm={3} md={2} className={classes.header}>
+            <Navbar />
         </Grid>
-        <Grid className={classes.main} item xs={12} sm={8} md={10}>
-          <img src={`${baseURL}5/run/pikrellcam/mjpeg.jpg?${time}`} width="90%" />
+        <Grid className={classes.main} item xs={12} sm={9} md={10}>
+          <h1 style={{ color: '#7e7e7e', fontWeight: 'bold', fontFamily: 'Segoe UI', margin: '0', width: '100%', textAlign: 'left' }}>Live</h1>
+          <div className={classes.video}>
+            <img src={`${baseURL}5/run/pikrellcam/mjpeg.jpg?${time}`} style={{ width: "100%", height: "100%" }} />
+          </div>
         </Grid>
       </Grid>
-    </Container>
+    </Container >
   );
 }
