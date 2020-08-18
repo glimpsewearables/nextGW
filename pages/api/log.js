4e95b67180caf5f66b9e42e37acd2b1f2b19bbfc
abdullahshah02 <abdullah.shah877@gmail.com>
2020-08-18 16:25:50 +0500

    updated to new tunnel url

--- pages/api/log.js
+++ pages/api/log.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default async (req, res) => {
 	try {
 		const { baseURL } = req.query;
-		const tunnel_path = `http://a1365c5ff9a4.ngrok.io/glimpse-cam/glimpseLog.log`;
+		const tunnel_path = `http://1d9cee56a95a.ngrok.io/home/pi/glimpse-cam/glimpseLog.log`;
 		const response = await axios.get(tunnel_path);
 		const log = response.data.split('\n').reverse();
 		const updated_log = log.map(string => { 
