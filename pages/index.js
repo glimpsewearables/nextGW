96f955412b7a41eba3a84bbc4ef3efea38703711
Muhammad Awais <chdryawais.ca@gmail.com>
2020-11-28 23:28:26 +0500

    font fixes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ export default function Home({ baseURL }) {
     <div>
       <Head>
         <title>WiFi App</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
       <Design baseURL={baseURL} />
     </div>
